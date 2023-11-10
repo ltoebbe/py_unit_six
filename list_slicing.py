8a9496ae87a0129b2a99303124bbcc30d3601275
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-11-10 14:12:51 -0500

    .

--- list_slicing.py
+++ list_slicing.py
@@ -1,34 +1,33 @@
-names = ["Abigail", "Brenda", "Chad", "Doug", "Emma", "Francis", "George", "Harold", "Imogen”",
-    "Jackie", "Kurt", "Linda"]
+names = ["Abigail", "Brenda", "Chad", "Doug", "Emma", "Francis", "George", "Harold", "Imogen",
+         "Jackie", "Kurt", "Linda"]
 
 # 1. Print ['Doug', 'Emma']
-print(names[3, 4])
-
+print(names[3:5])
 
 # 2. Print [‘Brenda’, ‘Chad’, ‘Doug’, ‘Emma’, ‘Francis’]
-
+print(names[1:6])
 
 # 3. [‘Francis’, ‘George’, ‘Harold’, ‘Imogen’, ‘Jackie’, ‘Kurt’, ‘Linda’] (using two numbers in the slice)
-
+print(names[5:12])
 
 # 4. [‘Francis’, ‘George’, ‘Harold’, ‘Imogen’, ‘Jackie’, ‘Kurt’, ‘Linda’] (using one number in the slice)
-
+print(names[5:])
 
 # 5. [‘Linda’] (using a positive number)
-
+print(names[11])
 
 # 6. [‘Linda’] (using a negative number)
-
+print(names[-1])
 
 # 7. [‘Brenda’, ‘Doug’, ‘Francis’, ‘Harold’]
-
+print(names[1], names[3], names[5], names[7])
 
 # 8. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’]
+print(names[0], names[2], names[4], names[6], names[8], names[10])
 
-
-# 9. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’] (using a different way than above.
-    # HINT: If you leave a slice number blank, the default is either the beginning or the end of the list,
-    # depending on which side of the colon is blank.
+# 9. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’] (using a different way than above.)
+# HINT: If you leave a slice number blank, the default is either the beginning or the end of the list,
+# depending on which side of the colon is blank.
 
 
 # 10. [‘Linda’, ‘Kurt’, ‘Jackie’]
@@ -38,4 +37,4 @@ print(names[3, 4])
 
 
 # 12. ['Linda', 'Kurt', 'Jackie', 'Imogen', 'Harold', 'George', 'Francis', 'Emma', 'Doug', 'Chad', 'Brenda', 'Abigail']
-    # (in a different way than 11. See hint for 9 for help.)
+# (in a different way than 11. See hint for 9 for help.)
