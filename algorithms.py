e491993d5beb5101dd71b3666ccb886419f0b3c5
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-11-17 14:13:35 -0500

    .

--- algorithms.py
+++ algorithms.py
@@ -13,16 +13,36 @@ def add_numbers(nums_to_sum):
         del nums_to_sum[0]
     return summed_nums
 
+
 print(add_numbers(nums_to_sum))
 
 
-def get_max(numbers):
+# ________________________________________________________________________________________________
+
+list2minmax = [45, 23, 99, 34, 67, 98, 0]
+
+
+def get_max(list2minmax):
     """
     Ex. get_max([45, 23, 99, 34, 67, 98, 0]) returns 99
-    :param numbers: a list of numbers
+    :param list2minmax: a list of numbers
     :return: The largest number in the list
     """
-    pass # remove this line when starting your function
+    for x in range(len(list2minmax) - 1):
+        if list2minmax[0] > list2minmax[1]:
+            del list2minmax[1]
+        elif list2minmax[0] < list2minmax[1]:
+            del list2minmax[0]
+    return list2minmax
+
+
+get_max(list2minmax)
+print(get_max(list2minmax))
+
+# ________________________________________________________________________________________________
+
+list2minmax = [45, 23, 99, 34, 67, 98, 0]
+
 
 def get_min(numbers):
     """
@@ -30,7 +50,22 @@ def get_min(numbers):
     :param numbers: a list of numbers
     :return: The smallest number in the list
     """
-    pass # remove this line when starting your function
+    for x in range(len(list2minmax) - 1):
+        if list2minmax[0] < list2minmax[1]:
+            del list2minmax[1]
+        elif list2minmax[0] > list2minmax[1]:
+            del list2minmax[0]
+    return list2minmax
+
+
+get_min(list2minmax)
+print(get_min(list2minmax))
+
+
+# ________________________________________________________________________________________________
+
+list1 = [3, 4, 7, 9]
+list2 = [1, 5, 8, 11]
 
 
 def merge(list1, list2):
@@ -40,4 +75,5 @@ def merge(list1, list2):
     :param list2: a second list in sorted order
     :return: a single list consisting of both smaller lists combined in sorted order.
     """
-    pass # remove this line when starting your function
+    list.sort(list1)
+    print(list1)
