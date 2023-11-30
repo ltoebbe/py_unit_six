670261a80638c3942bbacc9823ea8a9b1b566fd6
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-11-30 11:53:52 -0500

    .

--- assignment_six.py
+++ assignment_six.py
@@ -1,16 +1,32 @@
 # Liam Toebbe
 # Nov 30, 2023
 
+# text_file = open("text_file", "r")
+#
+# file_contents = text_file.readlines
+# print("file contents straight", file_contents)
+# line_count = len(file_contents)
+# print("line count: ", line_count)
+#
+# file_contents_str = str(file_contents)[1:-1]
+# print(file_contents_str)
+# word_units = file_contents_str.split(' ', -1)
+# print(word_units)
+# word_count = word_units.count(' ')
+# print("word count: ", word_count)
+
 text_file = open("text_file", "r")
 
-file_contents = text_file.readlines()                   # ['L1\n', 'L2, electric boogaloo\n', 'L3 (the threequel)\n', '\n', 'L5- four is blank']
-print("file contents straight", file_contents)
-line_count = len(file_contents)                         # 5
+file_contents = text_file.readlines()
+line_count = len(file_contents)
 print("line count: ", line_count)
 
-file_contents_str = str(file_contents)[1:-1]            # 'L1', 'L2, electric boogaloo', 'L3 (the threequel)', , 'L5- four is blank'
-print(file_contents_str)
-word_units = file_contents_str.split(' ', -1)      # 'L1', 'L2', 'electric', 'boogaloo', 'L3', '(the', 'threequel)', 'L5-', 'four', 'is', 'blank'
-print(word_units)
-word_count = word_units.count(' ')                      # 11
-print("word count: ", word_count)
+word_count = 0
+for line in text_file.readlines():
+    words = line.split()
+    word_count = word_count + len(words)
+print("wordcount: ", word_count)
+
+content = text_file.read()
+character_count = len(content)
+print(character_count)
